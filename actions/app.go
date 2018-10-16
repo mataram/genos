0b0350648b28ead23cd6d618a244276236efa7cd
ditect <parditech@gmail.com>
2018-10-16 14:45:57 +0000

    Add service resource

--- actions/app.go
+++ actions/app.go
@@ -49,6 +49,8 @@ func App() *buffalo.App {
 
 		app.GET("/", HomeHandler)
 
+		app.Resource("/services", ServicesResource{})
+		
 		app.ServeFiles("/", assetsBox) // serve files from the public directory
 	}
 
